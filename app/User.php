7ba38b9819afb5baa86938e3abe078030fc0034e
ahmed salah <ahmed salah>
2016-12-07 04:45:20 +0200

    admin master page setup

--- app/User.php
+++ app/User.php
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    // Role has many Users #RELATIONSHIP
+    public function Role(){
+        return $this->belongsTo('App\Role');
+    }
 }
